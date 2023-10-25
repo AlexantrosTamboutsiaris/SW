8ff1dc3d316c3cd174b8094c72962fbe91bd677a
AlexantrosTamboutsiaris <c3061355@newcastle.ac.uk>
2023-10-25 17:18:33 +0100

    Add density plot of character mass broken down by gender

--- analysis.R
+++ analysis.R
@@ -9,3 +9,7 @@ starwars %>%
   ggplot(aes(x = height, y = mass, col = species)) +
   geom_point()
 starwars %>% slice_max(mass)
+
+starwars %>% ggplot(aes(x = mass, fill = gender)) +
+  geom_density() +
+  scale_x_log10()
